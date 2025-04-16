108718888aad072d55de2cbfafb61f18016ea015
Vincent Zwingmann <vincent.zwingmann@gmail.com>
2025-04-16 23:27:18 +0200

    suppression du resizer

--- app/controllers/vetementForm.controller.ts
+++ app/controllers/vetementForm.controller.ts
@@ -119,13 +119,7 @@ export const pickImageForm = async (setForm: React.Dispatch<React.SetStateAction
         legacy: true
     });
     if (!result.canceled) {
-        await ImageResizer.createResizedImage(result.assets[0].uri, 250, 250, "JPEG", 90, 0).then((compressedImage) => {
-            // compress image will be low size which will be use to upload to server
-            setImageForm(compressedImage, setForm);
-          }).catch((err) => {
-            console.log("Erreur lors du redimensionnement", err);
-          });
-        
+        setImageForm(result.assets[0], setForm);
     }
 };
 /**
